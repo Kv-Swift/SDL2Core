3bc8685bc23bbd7377dfda49ffa21604eec81cdd
kivyswiftlink <kivyswiftlink@gmail.com>
2024-12-20 21:35:48 +0000

    Update from https://github.com/kivyswiftlink/CoreBuilder/commit/6ac70b219087f126103c6c54d23fccc7c31b7d85

--- Package.swift
+++ Package.swift
@@ -43,23 +43,23 @@ let package = Package(
         ),
         .binaryTarget(
             name: "libSDL2",
-            url: "https://github.com/kivyswiftlink/SDL2Core/releases/download/311.0.5/libSDL2.zip",
-            checksum: "8b4e14501ff2fc91c336d8a975ed0899b79a418f8a176e256028bf272705005c"
+            url: "https://github.com/kivyswiftlink/SDL2Core/releases/download/311.0.7/libSDL2.zip",
+            checksum: "08c944572b9f8dab16e7a2490aed2c23493cee5dd8c9e1cfcf24e916e0c8a793"
         ),
         .binaryTarget(
             name: "libSDL2_image",
-            url: "https://github.com/kivyswiftlink/SDL2Core/releases/download/311.0.5/libSDL2_image.zip",
-            checksum: "49521b0afb9cab11d431f894282746d3ce24ea648453d922f76d835e217846d8"
+            url: "https://github.com/kivyswiftlink/SDL2Core/releases/download/311.0.7/libSDL2_image.zip",
+            checksum: "375509acbb320b752b9d99d3e673461d2477b52845dbe2ef32996776bff15a89"
         ),
         .binaryTarget(
             name: "libSDL2_mixer",
-            url: "https://github.com/kivyswiftlink/SDL2Core/releases/download/311.0.5/libSDL2_mixer.zip",
-            checksum: "924b374abdfdbc449ce0c75d6686c622d5692b9a49a2fe3ee0bd0826ece6bc64"
+            url: "https://github.com/kivyswiftlink/SDL2Core/releases/download/311.0.7/libSDL2_mixer.zip",
+            checksum: "1851366e165b39a38aecf31908835a789f8104b12e562edfd995e1622514d2f0"
         ),
         .binaryTarget(
             name: "libSDL2_ttf",
-            url: "https://github.com/kivyswiftlink/SDL2Core/releases/download/311.0.5/libSDL2_ttf.zip",
-            checksum: "48c8f4b16b61420fd1f25e5ff7a1fe1718f79d8e1345524916fdfe380dd65d59"
+            url: "https://github.com/kivyswiftlink/SDL2Core/releases/download/311.0.7/libSDL2_ttf.zip",
+            checksum: "35ccce21edffc78807c7d63fcebaa2ecde2b62e4046babaf6cb02e4ba7718127"
         )
     ]
 )
